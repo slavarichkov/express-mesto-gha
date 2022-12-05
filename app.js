24367df8486c5473cb71213a57aa5bc93a31bb62
Slava Richkov <v.v.richkov@yandex.ru>
2022-12-05 09:30:02 +0500

    refactoring

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 // подключаемся к mongo и затем к серверу
 mongoose.connect('mongodb://127.0.0.1/mestodb', () => {
   console.log('DB OK');
-  app.listen(PORT, () => {
+  app.listen(3000, () => {
     console.log(`App listening on port ${PORT}`)
   })
 })
