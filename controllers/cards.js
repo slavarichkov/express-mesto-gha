59999192368ae9f7641a182fc1a3934d063048d0
Slava Richkov <v.v.richkov@yandex.ru>
2022-12-25 10:26:27 +0500

    fix: исправление обработки ошибок

--- controllers/cards.js
+++ controllers/cards.js
@@ -4,7 +4,6 @@ const card = require('../models/card');// импортируем модель(с
 const BAD_REQUEST_M = require('../utils/mist/BAD_REQUEST');
 const INTERNAL_SERVER_ERROR_M = require('../utils/mist/INTERNAL_SERVER_ERROR');
 const NOT_FOUND_M = require('../utils/mist/NOT_FOUND');
-const FORBIDDEN_M = require('../utils/mist/NOT_FOUND');
 
 const { OK } = require('../utils/constant'); // импортируем коды ошибок
 
@@ -14,15 +13,15 @@ const getAllCards = (req, res) => { // получить все карточки
     .catch(() => new INTERNAL_SERVER_ERROR_M('Произошла ошибка'));
 };
 
-const createCard = (req, res) => { // создать карточку
+const createCard = (req, res, next) => { // создать карточку
   const { name, link } = req.body; // получим из объекта запроса данные для карточки
   const owner = req.user._id; // получить айди юзера
   card.create({ name, link, owner }) // создать карточку в бд
     .then((newCard) => res.status(OK).send(newCard))
     .catch((err) => {
       if ((err.name === 'ValidationError')) {
-        throw new BAD_REQUEST_M('Переданы некорретные данные');
-      } else { throw new INTERNAL_SERVER_ERROR_M('Произошла ошибка'); }
+        next(new BAD_REQUEST_M('Переданы некорретные данные'));
+      } else { next(err); }
     });
 };
 
@@ -30,26 +29,28 @@ const deleteCard = (req, res, next) => {
   const { cardId } = req.params; // получим из объекта запроса уникальный id карточки
   card.findById(cardId)
     .then((cardFound) => {
-      if (!cardFound.owner.equals(req.user._id) || !cardFound) {
-        throw new FORBIDDEN_M('неверный id карточки или пользователя');
-      } else { cardFound.remove(cardId); }
+      if (!cardFound) {
+        throw new NOT_FOUND_M('неверный id карточки');
+      } else if (!cardFound.owner.equals(req.user._id)) {
+        throw new NOT_FOUND_M('неверный id пользователя');
+      } else { cardFound.remove(cardId).then(() => res.send({ message: 'Карточка удалена' })).catch(next); }
     })
     .catch(next);
 };
 
-const addLike = (req, res) => {
+const addLike = (req, res, next) => {
   card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
     { new: true },
   )
     .then((foundCard) => {
-      if (!foundCard) { res.status(NOT_FOUND_M).send({ message: 'Карточка с таким id не найдена' }); }
+      if (!foundCard) { throw new NOT_FOUND_M('Карточка с таким id не найдена'); }
       res.status(OK).send({ message: 'Выполнено' });
     })
     .catch((err) => {
       if (err.name === 'CastError') { throw new BAD_REQUEST_M('Передан некорректный id'); }
-      throw new INTERNAL_SERVER_ERROR_M('Произошла ошибка');
+      next(new INTERNAL_SERVER_ERROR_M('Произошла ошибка'));
     });
 };
 
